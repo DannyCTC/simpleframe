c49d7af4afd7cb82a6d3c05fb01b3cce8ff84134
Danny <edanny3@msn.com>
2019-11-01 16:25:27 +0800

    simpleframe ok

---
+++ simpleframe/frameclass.swift
@@ -0,0 +1,13 @@
+//
+//  frameclass.swift
+//  simpleframe
+//
+//  Created by Apple on 2019/11/1.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import Foundation
+
+open class FrameClass {
+    
+}
---
+++ simpleframe/simpleframe.h
@@ -0,0 +1,19 @@
+//
+//  simpleframe.h
+//  simpleframe
+//
+//  Created by Apple on 2019/11/1.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for simpleframe.
+FOUNDATION_EXPORT double simpleframeVersionNumber;
+
+//! Project version string for simpleframe.
+FOUNDATION_EXPORT const unsigned char simpleframeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <simpleframe/PublicHeader.h>
+
+
